f9d1da68b616609452ed81f6de1c50086d2e5858
Manish <mp626329@gmail.com>
2022-07-14 23:27:17 +0530

    more updation in gitignore

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -44,7 +44,7 @@ function Listing() {
     }
 
     return (
-        <main>
+        <main style={{marginBottom: '100px'}}>
             {/* <Helmet>
                 <title>{listing.name}</title>
             </Helmet> */}
@@ -148,6 +148,6 @@ function Listing() {
     )
 }
 
-export default Listing
+export default Listing;
 
 // https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
